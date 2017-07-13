339e7400b176f5438d62e86f24cfe8533268315d
Christophe Belpaire <christophe.belpaire@gmail.com>
2017-07-13 18:43:26 +0200

    Update nerves_io_nfc.ex
    Fix spacing

--- lib/nerves_io_nfc.ex
+++ lib/nerves_io_nfc.ex
@@ -73,8 +73,8 @@ defmodule Nerves.IO.NFC do
 
 
   defp restart(state) do
-    System.cmd ("killall", [state.nfc_poller])
-    executable = :code.priv_dir(:nerves_io_nfc)++ to_char_list("/#{state.nfc_poller}")
+    System.cmd("killall", [state.nfc_poller])
+    executable = :code.priv_dir(:nerves_io_nfc) ++ to_char_list("/#{state.nfc_poller}")
     port = Port.open({:spawn_executable, executable},
                      [{:args, []},
                       {:packet, 2},
